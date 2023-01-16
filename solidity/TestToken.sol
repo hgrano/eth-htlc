cbc0eb17274ca3937172ba487f6b0f27cccb9b7a
hgrano <huw.grano@gmail.com>
2023-01-17 10:44:23 +1100

    commenting test token

--- solidity/TestToken.sol
+++ solidity/TestToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// ERC20 token used for unit testing
 contract TestToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("TestToken", "TT") {
         _mint(msg.sender, initialSupply);
